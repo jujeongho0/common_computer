a844c0261192e8f063cb0bacc15396131ce8ffc2
Jeongho Ju <price_o@naver.com>
2022-05-25 22:05:25 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit_chat import message
+import requests
+
+st.header("Dialogpt를 이용한 영어 Chatbot")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+user_input = st.text_input("당신: ", key="input")
+
+if user_input:
+
+    url = "https://main-openchat-fpem123.endpoint.ainize.ai/send/jeongho"
+    data = {
+        "bot_id": "",
+        "topic": "",
+        "text": user_input,
+        "agent": "dialogpt.medium"
+    }
+
+    response = requests.post(url, data=data)
+    answer = response.json()['output']
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated']) - 1, -1, -1):
+        message(st.session_state['generated'][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
